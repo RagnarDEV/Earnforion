c834a9623514d214e140315c838257a2364c735a
hamad9iq <ahmed92alrahal@gmail.com>
2026-07-31 10:07:18 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Earnforion/',
   plugins: [react()],
-});
+  base: '/Earnforion/', // ✅ إضافة مسار المستودع هنا
+})
