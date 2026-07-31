60826d31e1f2aab2484a8b5cae6f9b214559099c
hamad9iq <ahmed92alrahal@gmail.com>
2026-07-31 09:52:14 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/earnforion/',
+  base: '/Earnforion/',
   plugins: [react()],
 });
