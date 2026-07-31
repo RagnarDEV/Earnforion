da5f1e848e82b6da77c217c61359ee5c12a94a92
hamad9iq <ahmed92alrahal@gmail.com>
2026-07-31 10:23:24 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import Navbar from './components/layout/Navbar';
-import Footer from './components/layout/Footer';
-import HomePage from './pages/HomePage';
+import React from 'react'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+import HomePage from './pages/HomePage'
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="min-h-screen bg-slate-900 text-slate-100 flex flex-col justify-between">
       <Navbar />
-      <div className="flex-1">
+      <main className="flex-grow">
         <HomePage />
-      </div>
+      </main>
       <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
