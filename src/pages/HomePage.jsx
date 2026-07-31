1e15f0c786d1fcac1acd85cf847f03291946e53a
hamad9iq <ahmed92alrahal@gmail.com>
2026-07-31 08:39:40 +0300

    Add HomePage component with Hero and Trending sections

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import HeroSection from '../components/home/HeroSection';
+import TrendingSection from '../components/home/TrendingSection';
+
+export default function HomePage() {
+  return (
+    <main>
+      <HeroSection />
+      <TrendingSection />
+      {/* ستُضاف باقي الأقسام لاحقاً */}
+    </main>
+  );
+}
